023194137e1596d6f8d3ece8727a4262947e169c
kunalsupekar <kunal.supekar@mitaoe.ac.in>
2024-04-16 09:57:25 +0530

    second day third

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+
+#!/bin/bash
+sudo yum update -y
+sudo yum install epel-release
+wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+sudo mkdir -p /opt/java17
+sudo tar -zxvf jdk-17_linux-x64_bin.tar.gz -C /opt/java17 --strip-components=1
+export JAVA_HOME=/opt/java17
+export PATH=$PATH:$JAVA_HOME/bin
+
+
 cd /home/ec2-user/server
 sudo java -jar -Dserver.port=80 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
